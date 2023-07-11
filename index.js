f22dceb25725f7b7341caa07593f507ca7aa1aa8
Nkambi Julius Forsuh <nkambijuliusforsuh@gmail.com>
2023-07-11 13:38:50 +0100

    first commit

--- index.js
+++ index.js
@@ -14,24 +14,46 @@
 
 
 /**how tot increment and decrement in javascript */
-var increment = 6, decrement = 10;
+//var increment = 6, decrement = 10;
 
-console.log(increment, decrement)
+//console.log(increment, decrement)
 
 //increment = increment + 1
-increment++
+//increment++
 
 //decrement = decrement + 1
-decrement--
+//decrement--
 
-console.log(increment, decrement)
+//console.log(increment, decrement)
 // exaamples
 //1)
 //temp= ++increment +2 //for pre-incrementation we increment the value before we use it 
-console.log(temp)
+//console.log(temp)
 //2)
-temp2= 2 + increment++  //for post-incrementation we decrement the value after we use it 
-console.log(temp2)
+//temp2= 2 + increment++  //for post-incrementation we decrement the value after we use it 
+//console.log(temp2)
+
+
+/*working on strings********************************************************/
+
+/*working with arrays*******************************************************/
+var variable = 1
+const myarr = [23,45,"hello",null,true, ["yo",1,2], variable]
+
+console.log(myarr)
+myarr[2] = 'bonjour'
+
+//myarr.pop()/*pop is used to remove the last elementin the array*/
+//myarr.shift()/*shift is used to remove the element element in the array*/
+//myarr.unshift('bonjour')/*unshift bring the element to the index position 1*/
+
+//myarr.map((i, j)=>{      //were i is the element in the array while j is the index position of the element in the array
+ //   console.log("index: " + j + " ,value" +i)
+//})
+//myarr.sort()/*help to sort the array*/
+console.log(myarr.some)
+console.log(myarr)
+
 
 
 
