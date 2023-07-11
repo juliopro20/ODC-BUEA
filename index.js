6f71b31d671ce9c1da8660d34b9d05faa64fa9e2
julopro20 <nkambijuliusforsuh@gmail.com>
2023-07-11 11:30:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//var lo = 8;
+
+//console.log(lo)
+
+
+
+
+//var a=2, b=10;
+//console.log(a + b)
+//console.log(a - b)
+//console.log(a * b)
+//console.log(a / b)
+//console.log(a % b)
+
+
+/**how tot increment and decrement in javascript */
+var increment = 6, decrement = 10;
+
+console.log(increment, decrement)
+
+//increment = increment + 1
+increment++
+
+//decrement = decrement + 1
+decrement--
+
+console.log(increment, decrement)
+// exaamples
+//1)
+//temp= ++increment +2 //for pre-incrementation we increment the value before we use it 
+console.log(temp)
+//2)
+temp2= 2 + increment++  //for post-incrementation we decrement the value after we use it 
+console.log(temp2)
+
+
+
+
